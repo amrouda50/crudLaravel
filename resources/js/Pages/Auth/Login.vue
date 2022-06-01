c3a5857849a598be00dd37b45b87d272e433ebd3
Omar Hisham Aly Abdelmeguid Ashour <e5dzst@inf.elte.hu>
2022-06-01 23:19:19 +0200

    HTML and styling done for all application components

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,7 +1,22 @@
 <template>
     <div>
         <nav-bar/>
+        <form action="action_page.php" method="post">
+            <div class="imgcontainer">
+                <img :src="require('../../../assets/img_avatar.png').default" alt="Avatar" class="avatar">
+            </div>
 
+            <div class="container">
+                <label for="uname"><b>Email</b></label>
+                <input type="text" placeholder="Enter Email" name="uname" required>
+
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required>
+
+                <button type="submit">Login</button>
+
+            </div>
+        </form>
     </div>
 </template>
 
@@ -16,6 +31,49 @@ export default {
 </script>
 
 <style>
+form {
+    border: 3px solid #f1f1f1;
+}
+
+input[type=text], input[type=password] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+button {
+    background-color: #04AA6D;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+}
+
+button:hover {
+    opacity: 0.8;
+}
+
+
+.imgcontainer {
+    text-align: center;
+    margin: 24px 0 12px 0;
+}
+
+
+img.avatar {
+    width: 40%;
+    border-radius: 50%;
+}
+
+
+.container {
+    padding: 16px;
+}
 
 
 </style>
