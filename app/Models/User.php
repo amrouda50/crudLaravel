f9079ec4b03818f725e8654b0aacc93ea939ad4d
Omar Hisham Aly Abdelmeguid Ashour <e5dzst@inf.elte.hu>
2022-06-01 20:24:48 +0200

    Database creation and Users Seeded

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,8 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
+    public static $ADMIN = 'ADMIN';
+    public static $NORMAL_USER = 'NORMAL_USER';
     /**
      * The attributes that are mass assignable.
      *
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use App\Models\User;
 class CreateUsersTable extends Migration
 {
     /**
@@ -17,10 +17,12 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('type')->default(User::$NORMAL_USER);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
         });
     }
 
